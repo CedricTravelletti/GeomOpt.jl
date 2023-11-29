560598d5f6a0b5b87a081d773287d304c723eada
Documenter.jl <documenter@juliadocs.github.io>
2023-11-29 10:48:34 +0000

    build based on 017cba3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GeomOpt","category":"page"},{"location":"#GeomOpt","page":"Home","title":"GeomOpt","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GeomOpt.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GeomOpt]","category":"page"}]
+}
