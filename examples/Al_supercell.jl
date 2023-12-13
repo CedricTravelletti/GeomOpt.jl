45e90e18270f195cd27dff8c4cdc8e9741d9def9
CedricTravelletti <cedrictravelletti@gmail.com>
2023-12-13 09:02:41 +0100

    Removed x0 functionality in optimize_geometry.

--- examples/Al_supercell.jl
+++ examples/Al_supercell.jl
@@ -1,10 +1,10 @@
-using DFTK: AtomsCalculators
 #= Test Geometry Optimization on an aluminium supercell.
 =#
 using Printf
 using LinearAlgebra
 using DFTK
 using ASEconvert
+using AtomsCalculators
 using Unitful
 using UnitfulAtomic
 using Random
@@ -35,26 +35,23 @@ end;
 
 al_supercell = build_al_supercell(1)
 
-# Set everything to optimizable.
-al_supercell = clamp_atoms(al_supercell, nothing)
-
 # Create a simple calculator for the model.
-calculator = DFTKCalculator(al_supercell; Ecut=30.0, kgrid=[7, 7, 7], tol=1e-6, temperature=1e-4, verbose_scf=true)
+calculator = DFTKCalculator(al_supercell; Ecut=20.0, kgrid=[5, 5, 5], tol=1e-6, temperature=1e-4, verbose_scf=true)
+
+energy_true = AtomsCalculators.potential_energy(al_supercell, calculator)
 
 # Starting position is a random perturbation of the equilibrium one.
 Random.seed!(1234)
-x0 = vcat(get_positions_cart(al_supercell)...)
+x0 = vcat(position(al_supercell)...)
 σ = 0.5u"angstrom"; x0_pert = x0 + σ * rand(Float64, size(x0))
+update_optimizable_coordinates(al_supercell, x0_pert)
+energy_pert = AtomsCalculators.potential_energy(al_supercell, calculator)
 
-energy_true = AtomsCalculators.potential_energy(al_supercell, calculator)
-energy_pert = AtomsCalculators.potential_energy(
-                update_optimizable_coordinates_cart(al_supercell, x0_pert), calculator, calculator.state)
 @printf "Initial guess distance (norm) from true parameters %.3e bohrs.\n" austrip(norm(x0 - x0_pert))
 @printf "Initial regret %.3e.\n" energy_pert - energy_true
 
 method = Optim.LBFGS()
-optim_options = Optim.Options(f_tol=1e-32, iterations=10, show_trace=true,extended_trace=true)
+optim_options = Optim.Options(f_tol=1e-32, iterations=5, show_trace=true,extended_trace=true)
 
-results = optimize_geometry(al_supercell, calculator, austrip.(x0_pert);
-            method=method, optim_options=optim_options)
+results = optimize_geometry(al_supercell, calculator; method=method, optim_options=optim_options)
 println(results)
